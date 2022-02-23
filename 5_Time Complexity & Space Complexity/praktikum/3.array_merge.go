4efabc2ae4e8340f71cacb520ed2fefc0328dcf1
Restu Aditya Rachman <restuarachman@gmail.com>
2022-02-23 08:30:32 +0700

    add section 5

---
+++ 5_Time Complexity & Space Complexity/praktikum/3.array_merge.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func ArrayMerge(arrayA, ArrayB []string) []string {
+
+}
+
+func main() {
+	// Test cases
+	fmt.Println(ArrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
+	// ["king", "devil jin", "akuma", "eddie", "steve", "geese"]
+
+	fmt.Println(ArrayMerge([]string{"sergei", "jin"}, []string{"jin", "steve", "bryan"}))
+	// ["sergei", "jin", "steve", "bryan"]
+
+	fmt.Println(ArrayMerge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa", "law"}))
+	// ["alisa", "yoshimitsu", "devil jin", "law"]
+
+	fmt.Println(ArrayMerge([]string{}, []string{"devil jin", "sergei"}))
+	// ["devil jin", "sergei"]
+
+	fmt.Println(ArrayMerge([]string{"hwoarang"}, []string{}))
+	// ["hwoarang"]
+
+	fmt.Println(ArrayMerge([]string{}, []string{}))
+	// []
+}
