a61f6f090f1923b5d126874fdd2fe360c66e8b4d
Restu Aditya Rachman <restuarachman@gmail.com>
2022-02-24 17:47:26 +0700

    add solution for problem 3,4,5 in section 5

---
+++ 5_Time Complexity & Space Complexity/praktikum/4.angka_muncul_sekali.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func angkaMunculSekali(angka string) []int {
+	count := map[string]int{}
+	for _, value := range angka {
+		count[string(value)] += 1
+	}
+
+	result := []int{}
+	for key, val := range count {
+		if val == 1 {
+			num, _ := strconv.Atoi(key)
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(angkaMunculSekali("1234123"))
+}
---
+++ 5_Time Complexity & Space Complexity/praktikum/5.pair_with_target_sum.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func PairSum(arr []int, target int) []int {
+	hash := map[int]int{}
+	result := []int{}
+
+	for i, value := range arr {
+		if pos, exist := hash[target-value]; exist && i != pos {
+			result = append(result, hash[target-value], i)
+		} else {
+			hash[value] = i
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(PairSum([]int{1, 2, 3, 4, 6}, 6))
+	/*
+	   [1, 3]
+	*/
+}
