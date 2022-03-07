ef31faf8dc136311d8db1a995b698a60900c387d
Restu Aditya Rachman <restuarachman@gmail.com>
2022-03-07 14:26:39 +0700

    edit problem 4

--- 9_Brute Force - Greedy and Divide and Conquer/praktikum/4.binary_search.go
+++ 9_Brute Force - Greedy and Divide and Conquer/praktikum/4.binary_search.go
@@ -5,7 +5,7 @@ import "fmt"
 func BinarySearch(array []int, x int) {
 	left := 0
 	right := len(array) - 1
-	mid := left + right/2
+	mid := (left + right) / 2
 	for left < right && array[mid] != x {
 
 		if x < array[mid] {
@@ -13,7 +13,7 @@ func BinarySearch(array []int, x int) {
 		} else if x > array[mid] {
 			left = mid + 1
 		}
-		mid = left + right/2
+		mid = (left + right) / 2
 	}
 	if array[mid] == x {
 		fmt.Println(mid)
@@ -24,7 +24,7 @@ func BinarySearch(array []int, x int) {
 
 func main() {
 	BinarySearch([]int{1, 1, 3, 5, 5, 6, 7}, 3)
-	BinarySearch([]int{1, 2, 3, 4, 6, 8, 10}, 5)
+	BinarySearch([]int{1, 2, 3, 5, 6, 8, 10}, 5)
 	BinarySearch([]int{12, 15, 15, 19, 24, 31, 53, 59, 60}, 53)
 	BinarySearch([]int{12, 15, 15, 19, 24, 31, 53, 59, 60}, 100)
 }
