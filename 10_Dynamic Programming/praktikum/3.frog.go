f31002d43ac0728d766656ec759309b47eae0098
Restu Aditya Rachman <restuarachman@gmail.com>
2022-03-09 17:40:58 +0700

    update problem 1 in section 10

--- 10_Dynamic Programming/praktikum/3.frog.go
+++ 10_Dynamic Programming/praktikum/3.frog.go
@@ -8,9 +8,11 @@ import (
 var dp = make(map[int]int)
 
 func Frog(jumps []int) int {
-	dp[0] = int(math.Abs(float64(jumps[1] - jumps[0])))
+	dp[0] = 0
+	dp[1] = int(math.Abs(float64(jumps[1] - jumps[0])))
 	for i := 2; i < len(jumps); i++ {
 		dp[i] = int(math.Min(math.Abs(float64(jumps[i]-jumps[i-1]))+float64(dp[i-1]), math.Abs(float64(jumps[i]-jumps[i-2]))+float64(dp[i-2])))
+
 	}
 	return dp[len(jumps)-1]
 }
