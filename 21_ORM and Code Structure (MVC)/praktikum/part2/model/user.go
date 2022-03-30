6752effc4f124aa48f987561f200475cf25ca51e
restuarachman <restuarachman@gmail.com>
2022-03-30 17:34:06 +0700

    edit struct

--- 21_ORM and Code Structure (MVC)/praktikum/part2/model/user.go
+++ 21_ORM and Code Structure (MVC)/praktikum/part2/model/user.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Author string `json:"author" form:"author"`
-	Title  string `json:"title" form:"title"`
-	Price  int    `json:"price" form:"price"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
 }
